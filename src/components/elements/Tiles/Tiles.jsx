9172aea5fb7e299633ab2f5dc33d0a14123de943
Maria Magnell <maria.magnell@gmail.com>
2025-10-17 16:00:37 +0200

    created tiles component

---
+++ src/components/elements/Tiles/Tiles.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Tiles.css'
+
+const Tiles = () => {
+  return (
+    <div>
+        <div className="tiles-card">
+                <div className="tiles-title">
+                    <h5>Title</h5>
+                </div>
+                <div className="tiles-text">
+                    <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.</span>
+                </div>                
+        </div> 
+    </div>
+  )
+}
+
+export default Tiles
