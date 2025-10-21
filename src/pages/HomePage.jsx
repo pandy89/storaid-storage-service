a6603d037df571480ba80d0655de5026c74226e8
Maria Magnell <maria.magnell@gmail.com>
2025-10-21 13:52:23 +0200

    HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,8 +7,8 @@ const HomePage = () => {
 
     return (        
         <div>
-        {/* <HeroMainPage /> */}
-        <HeroSubPage />
+        <HeroMainPage />
+        
             
         </div>
     )
