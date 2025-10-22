792298ff13d3000c68b2dbcd58b080eb9b3de0d7
Maria Magnell <maria.magnell@gmail.com>
2025-10-22 15:26:56 +0200

    read tiles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,13 @@ import HeroMainPage from '../components/sections/Hero/HeroMainPage';
 import HeroSubPage from '../components/sections/Hero/HeroSubPage';
 import AboutUs from '../components/sections/AboutUs/AboutUs';
 import Brand from '../components/sections/Brand/Brand';
+import Tiles from '../components/elements/Tiles/Tiles';
 
 const HomePage = () => {
 
     return (
-        <div>HomePage
-            <Tiles title="Title" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium." variant="secondary" />
+        <div>
+            <Tiles title="Title" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium." variant="primary" />
         </div>
     )
 }
