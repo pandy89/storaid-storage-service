0242dc6f6952ddf0e5a43050659cbe26bca2514d
Maria Magnell <maria.magnell@gmail.com>
2025-10-22 19:25:30 +0200

    our-service-section done.

--- src/components/sections/OurServices/OurServices.jsx
+++ src/components/sections/OurServices/OurServices.jsx
@@ -13,11 +13,11 @@ const OurServices = () => {
                     <span className="our-service-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</span>
                 </div>
                 <div className="our-service-content">
-                    <Tiles title="Diverse Unit Sizes" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-1" variant="secondary"/>
-                    <Tiles title="Vehicle Storage" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-2" variant="secondary"/>
+                    <Tiles title="Diverse Unit Sizes" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-1" variant="secondary" titleVariant="secondary" textVariant="secondary"/>
+                    <Tiles title="Vehicle Storage" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-2" variant="secondary" titleVariant="secondary" textVariant="secondary"/>
                     <div class="placeholder-box"></div>
-                    <Tiles title="Moving Assistance" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-3" variant="secondary"/>
-                    <Tiles title="Top-Notch Security" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-4" variant="secondary"/>
+                    <Tiles title="Moving Assistance" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-3" variant="secondary" titleVariant="secondary" textVariant="secondary"/>
+                    <Tiles title="Top-Notch Security" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." className="tile-4" variant="secondary" titleVariant="secondary" textVariant="secondary"/>
                 </div>
             </div>
         </div>
