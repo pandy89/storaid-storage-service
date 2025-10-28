c3642e0cf72b61fff00898233feefc8603b94402
Maria Magnell <maria.magnell@gmail.com>
2025-10-28 10:53:44 +0100

    Updated About us with section components.

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import HeroSubPage from '../components/sections/Hero/HeroSubPage';
 import AboutUs from '../components/sections/AboutUs/AboutUs'
+import Brand from '../components/sections/Brand/Brand';
+import Testimonials from '../components/sections/Testimonials/Testimonials';
+import WhyUs from '../components/sections/WhyChooseUS/WhyUs';
+import Subscribe from '../components/sections/Subscribe/Subscribe';
+
 
 const AboutUsPage = () => {
   return (
-    <div>
-        <HeroSubPage title="About Us" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
-        <AboutUs />
-    </div>
+    <>
+      <HeroSubPage title="About Us" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+      <AboutUs />
+      <Brand />
+      <Testimonials />
+      <WhyUs/>
+      <Subscribe />
+    </>
   )
 }
 
