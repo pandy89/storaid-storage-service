f9b2d67fbf92873151de9d45414a4c7c2559bdbd
Maria Magnell <maria.magnell@gmail.com>
2025-10-28 15:52:03 +0100

    Updates in contact us. Halfdone. Missing FAQ

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSubPage from '../components/sections/Hero/HeroSubPage'
 import GetInTouch from '../components/sections/GetInTouch/GetInTouch'
 import FindUsOn from '../components/sections/FindUsOn/FindUsOn'
+import Subscribe from '../components/sections/Subscribe/Subscribe'
 
 
 
@@ -9,11 +10,11 @@ import FindUsOn from '../components/sections/FindUsOn/FindUsOn'
 const ContactPage = () => {
   return (
     <div>
-      <HeroSubPage title="About Us" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."  />
+      <HeroSubPage title="Contact Us" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."  />
       <GetInTouch />
       <FindUsOn />
-
       {/* FAQ */}
+      <Subscribe /> 
     </div>
   )
 }
