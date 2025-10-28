c63230d659a0da2efd54ec4a7d0e77eeb89db260
Maria Magnell <maria.magnell@gmail.com>
2025-10-28 10:59:30 +0100

    Services page half done. FAQ missing

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import HeroSubPage from '../components/sections/Hero/HeroSubPage'
+import OurServices from '../components/sections/OurServices/OurServices'
+import Testimonials from '../components/sections/Testimonials/Testimonials'
+import Booking from '../components/sections/Booking/Booking'
+import Subscribe from '../components/sections/Subscribe/Subscribe'
 
 
 const ServicePage = () => {
   return (
-    <div>
-      <HeroSubPage title="Services" 
-        text="Vi är ett kreativt team som bygger lösningar."  />
-    </div>
+    <>
+      <HeroSubPage title="Services" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."  />
+      <OurServices />
+      <Testimonials />
+      {/* FAQ */}
+      <Booking />
+      <Subscribe />
+    </>
   )
 }
 
