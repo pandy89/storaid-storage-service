783536e009380075e7efe1f97ec815e9fb850077
Maria Magnell <maria.magnell@gmail.com>
2025-10-28 20:29:35 +0100

    Booking section half done. Missing FAQ

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import HeroSubPage from '../components/sections/Hero/HeroSubPage'
+import WhyUS from '../components/sections/WhyChooseUS/WhyUs'
+import Booking from '../components/sections/Booking/Booking'
+import Subscribe from '../components/sections/Subscribe/Subscribe'
 
 
 const BookingPage = () => {
   return (
-    <div><HeroSubPage title="Booking" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
-    </div>
+    <>
+    <HeroSubPage title="Booking" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+    {/* BookingUnit */}
+    <WhyUS />
+    <Booking /> 
+    {/* FAQ */}
+    <Subscribe />
+    </>
   )
 }
 
