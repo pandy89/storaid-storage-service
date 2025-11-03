8cce58ca5e25e294abe4392e2a5357a2f861da47
Maria Magnell <maria.magnell@gmail.com>
2025-11-03 10:22:52 +0100

    Accordioncomponent done.

---
+++ src/components/elements/AccordionItem.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const AccordionItem = ({item}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleAccordion = () =>{
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="questions-answer__accordion">
+        <div className={`question ${isOpen ? 'active': ''}`} onClick={toggleAccordion}>
+            <span className="title__question">{item.title}</span>
+            <svg className="chevron" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2"/></svg>
+        </div>
+        {
+          isOpen && (
+            <div className={`answer ${isOpen ? 'active': ''}`}>{item.description}</div>
+          )
+        }        
+    </div>
+  )
+}
+
+
+
+export default AccordionItem
--- src/components/elements/Faq.jsx
+++ src/components/elements/Faq.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import AccordionItem from './AccordionItem'
 
 const Faq = () => {
+   const [faqs, setFaqs] = useState([])
+  
+    const getFaqs = async () => {
+      const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/faqs')
+      const data = await res.json()
+      setFaqs(data)
+    }
+  
+    useEffect(() => {
+      getFaqs()
+    }, [])
+
   return (
-    <div>Faq</div>
+    <div>
+      <div className="accordion__wrapper">
+        <div className="questions__accordion">
+            {
+              faqs.map((faqs) => (
+                <AccordionItem key={faqs.id} item={faqs}/>
+              ))
+            }
+        </div>
+    </div> 
+    </div>
   )
 }
 
