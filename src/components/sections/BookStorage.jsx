14e9deb71884f091c0e6148f5cd2de59762d1f01
Maria Magnell <maria.magnell@gmail.com>
2025-11-12 15:33:13 +0100

    change buttonlink in booking

--- src/components/sections/BookStorage.jsx
+++ src/components/sections/BookStorage.jsx
@@ -7,7 +7,7 @@ const Booking = () => {
             <div className="booking-area">
                <h3 className="booking-headline">Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</h3> 
                <span className="booking-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</span>
-               <div className="booking-button-box"><Button name="Book Now" to="/about" aria-label="Book your storeage now."/></div>
+               <div className="booking-button-box"><Button name="Book Now" to="/booking" aria-label="Book your storeage now."/></div>
             </div>
         </div>
     </section>
